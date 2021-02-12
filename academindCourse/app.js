e0dfe86d97a0cefe086881e92626889e72b5b2f6
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-02-12 21:16:28 +0100

    enumeration

--- academindCourse/app.js
+++ academindCourse/app.js
@@ -1,5 +1,26 @@
+// const person: {
+//     name: string,
+//     age: number
+// }={
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "jonas",
-    age: 29
+    age: 29,
+    hobbies: ["coding", "beer", "thee"],
+    role: [2, 'author'],
+    enumRole: Role.ADMIN
 };
-console.log(person);
+person.role.push('admin');
+// person.role[1] = 10;
+var favoriteActivities; // can use any type to have lists wiht all types of objects
+favoriteActivities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+console.log(person.name);
--- academindCourse/app.ts
+++ academindCourse/app.ts
@@ -1,9 +1,31 @@
-const person: {
-    name: string,
-    age: number
+// const person: {
+//     name: string,
+//     age: number
+// }={
+
+enum Role {ADMIN = 0, READ_ONLY = 1, AUTHOR = 2}
+
+const person : {
+    name: string,           // string
+    age: number,            // number
+    hobbies: string[],      // array
+    role: [number, string], // typle
+    enumRole: Role
 }={
     name: "jonas",
-    age: 29
+    age: 29,
+    hobbies: ["coding", "beer", "thee"],
+    role: [2, 'author'],
+    enumRole: Role.ADMIN
 };
 
+person.role.push('admin');
+// person.role[1] = 10;
+
+let favoriteActivities: string[]; // can use any type to have lists wiht all types of objects
+favoriteActivities = ['Sports'];
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
 console.log(person.name);
