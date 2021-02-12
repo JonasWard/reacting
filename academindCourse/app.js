eff9075435433d76c8ae28cd18f0f29c5e57f306
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-02-12 21:04:45 +0100

    object types

--- academindCourse/app.js
+++ academindCourse/app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, printResult, phrase) {
-    if (printResult) {
-        console.log(phrase + (n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var printString = "fuck me: ";
-var result = add(number1, number2, printResult, printString);
-console.log(result);
+var person = {
+    name: "jonas",
+    age: 29
+};
+console.log(person);
--- academindCourse/app.ts
+++ academindCourse/app.ts
@@ -1,16 +1,9 @@
-function add(n1: number, n2 :number, printResult: boolean, phrase: string) {
-    if (printResult) {
-        console.log(phrase+(n1+n2) )
-    } else {
-        return n1+n2;
-    }
-    
-}
+const person: {
+    name: string,
+    age: number
+}={
+    name: "jonas",
+    age: 29
+};
 
-const number1: number= 5;
-const number2 = 2.8;
-const printResult = true;
-const printString = "fuck me: ";
-
-const result = add(number1, number2, printResult, printString);
-console.log(result);
+console.log(person.name);
